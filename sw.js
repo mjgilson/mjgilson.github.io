c740ca23732d7a5cc06f5c597e20fdd9de6729d6
Matt Gilson <31619762+mjgilson@users.noreply.github.com>
2023-04-26 09:04:06 +0100

    Cache all content

--- sw.js
+++ sw.js
@@ -1,18 +1,37 @@
 const CACHE_NAME = 'ohmuralPWA-cache-v1';
 
-const PRECACHE_ASSETS = [
-    'assets/',
-    'css/',
-    'fonts/',
-    'js/',
+const contentToCache = [
+    'index.html',
     'browserconfig.xml',
-    'manifest.json'
-]
+    'manifest.json',
+    'css/mural.css',
+    'js/audio.js',
+    'js/modal.js',
+    'js/scene-text.js',
+    'js/touch.js',
+    'fonts/MerriweatherSans-VariableFont_wght.ttf',
+    'assets/images/full-mural.jpg',
+    'assets/icons/android-chrome-192x192.png',
+    'assets/icons/android-chrome-512x512.png',
+    'assets/icons/apple-touch-icon.png',
+    'assets/icons/audio-icon.svg',
+    'assets/icons/favicon.ico',
+    'assets/icons/favicon-16x16.png',
+    'assets/icons/favicon-32x32.png',
+    'assets/icons/mstile-150x150.png',
+    'assets/icons/next-icon.svg',
+    'assets/icons/previous-icon.svg',
+    'assets/icons/safari-pinned-tab.svg'
+];
+for (let i = 1; i <= 46; i++) {
+    contentToCache.push('assets/images/scene-' + i + '.jpg');
+    contentToCache.push('assets/audio/scene-' + i + '.mp3');
+}
 
 self.addEventListener('install', event => {
     event.waitUntil((async () => {
         const cache = await caches.open(CACHE_NAME);
-        cache.addAll(PRECACHE_ASSETS);
+        cache.addAll(contentToCache);
     })());
 });
 
